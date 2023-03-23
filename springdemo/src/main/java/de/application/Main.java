a2ddd4b8b63b49e7f405e2fbcaeed7b607417049
Jo Wagner <jowagner@limago.de>
2023-03-23 13:50:34 +0100

    Intial commit

--- springdemo/src/main/java/de/application/Main.java
+++ springdemo/src/main/java/de/application/Main.java
@@ -1,20 +1,21 @@
 package de.application;
 
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.ComponentScan;
+
+
+@ComponentScan(basePackages = "de")
 public class Main {
-
 
 
 	public static void main(String[] args) {
 
 
-		final ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-
-
-
+		//final AbstractApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+		final AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(Main.class);
+		context.registerShutdownHook();
 
 	}
 
