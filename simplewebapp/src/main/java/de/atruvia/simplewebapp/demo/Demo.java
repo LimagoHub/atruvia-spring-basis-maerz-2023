a62a1b3c8710d24556769fce2a31b71f89a962f7
Jo Wagner <jowagner@limago.de>
2023-03-24 11:59:24 +0100

    config

--- simplewebapp/src/main/java/de/atruvia/simplewebapp/demo/Demo.java
+++ simplewebapp/src/main/java/de/atruvia/simplewebapp/demo/Demo.java
@@ -3,22 +3,25 @@ package de.atruvia.simplewebapp.demo;
 
 import de.atruvia.simplewebapp.repository.SchweinRepository;
 import de.atruvia.simplewebapp.repository.entity.SchweinEntity;
+import de.atruvia.simplewebapp.service.BlacklistService;
+import de.atruvia.simplewebapp.service.Person;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.annotation.PostConstruct;
 import java.util.UUID;
 
-@Component
+@Service
 @Transactional
 public class Demo {
 
-    private final SchweinRepository repo;
 
+    private final BlacklistService blacklistService;
     private String message = "Hallo Welt"; // 2. FieldInjection
-    public Demo(@Value("${Demo.gruss}") String message, SchweinRepository repo) {  // 1. Construktor
-        this.repo = repo;
+    public Demo(@Value("${Demo.gruss}") String message, BlacklistService blacklistService) {  // 1. Construktor
+        this.blacklistService = blacklistService;
         this.message = message;
         System.out.println("Ctor Demo");
         System.out.println(message);
@@ -27,10 +30,11 @@ public class Demo {
     @PostConstruct
     public void init() { // 3. PostConstruct
         System.out.println(message);
-
-        SchweinEntity piggy = SchweinEntity.builder().id(UUID.randomUUID()).name("Miss Piggy").gewicht(10).build();
-        repo.save(piggy);
-
+        Person p = new Person();
+        p.setVorname("Peter");
+        System.out.println(String.format("%s isBacklisted=%s", p, blacklistService.isBlacklisted(p)));
+        p.setVorname("FRanz");
+        System.out.println(String.format("%s isBacklisted=%s", p, blacklistService.isBlacklisted(p)));
 
     }
 }
